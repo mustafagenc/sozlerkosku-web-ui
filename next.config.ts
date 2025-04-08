b8e6bf29d890c7b8bdcbd38760b865dbb651264f
Mustafa Genc <eposta@mustafagenc.info>
2025-04-08 12:17:32 +0300

    old links have been redirected to the new ones

--- next.config.ts
+++ next.config.ts
@@ -1,5 +1,5 @@
-import type { NextConfig } from "next";
-import createNextIntlPlugin from "next-intl/plugin";
+import type { NextConfig } from 'next';
+import createNextIntlPlugin from 'next-intl/plugin';
 
 const withNextIntl = createNextIntlPlugin();
 const nextConfig: NextConfig = {
@@ -7,12 +7,61 @@ const nextConfig: NextConfig = {
   images: {
     remotePatterns: [
       {
-        protocol: "https",
-        hostname: "*",
-        port: "",
-        pathname: "/**",
+        protocol: 'https',
+        hostname: '*',
+        port: '',
+        pathname: '/**',
       },
     ],
+  },
+  async redirects() {
+    return [
+      {
+        source: '/5te5-namaz-aliskanlik-uygulamasi.html',
+        destination: '/applications/beste5v2/',
+        permanent: true,
+      },
+      {
+        source: '/lamelif-kuran-ve-tecvid-egitimi.html',
+        destination: '/applications/lamelif/',
+        permanent: true,
+      },
+      {
+        source: '/kopgit-risale-i-nur-okuma-programi.html',
+        destination: '/applications/kopgit/',
+        permanent: true,
+      },
+      {
+        source: '/iletisim.html',
+        destination: '/contact/',
+        permanent: true,
+      },
+      {
+        source: '/hakkimizda.html',
+        destination: '/about/',
+        permanent: true,
+      },
+      {
+        source: '/en-cok-merak-edilenler.html',
+        destination: '/',
+        permanent: true,
+      },
+      {
+        source: '/dava-okulu.html',
+        destination: '/',
+        permanent: true,
+      },
+      {
+        source: '/blog-sokak-roportaji.html',
+        destination: '/',
+        permanent: true,
+      },
+      {
+        source: '/blog-genclik-merkezimiz.html',
+        destination: '/',
+        permanent: true,
+      },
+    ];
   },
 };
 
